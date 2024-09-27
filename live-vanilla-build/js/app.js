1a1037dbf70f85006710770ef1299150e9e1d65c
ttkgns <ttkgns@gmail.com>
2024-09-27 23:06:12 +0900

    modified:   live-vanilla-build/css/index.css 	modified:   live-vanilla-build/index.html 	new file:   live-vanilla-build/js/app.js

---
+++ live-vanilla-build/js/app.js
@@ -0,0 +1,89 @@
+const App = {
+  // All of our selected HTML elements
+  $: {
+    menu: document.querySelector('[data-id="menu"]'),
+    menuItems: document.querySelector('[data-id="menu-items"]'),
+    resetBtn: document.querySelector('[data-id="reset-btn"]'),
+    newRoundBtn: document.querySelector('[data-id="new-round-btn"]'),
+    squares: document.querySelectorAll('[data-id="square"]'),
+  },
+
+  state: {
+    moves: [],
+  },
+
+  init() {
+    App.registerEventListeners();
+  },
+
+  registerEventListeners() {
+    // DONE
+    console.log(App.$.squares);
+    App.$.menu.addEventListener("click", (event) => {
+      App.$.menuItems.classList.toggle("hidden");
+    });
+
+    // TODO
+    App.$.resetBtn.addEventListener("click", (event) => {
+      console.log("Reset the game");
+    });
+
+    // TODO
+    App.$.newRoundBtn.addEventListener("click", (event) => {
+      console.log("Add a new round");
+    });
+
+    // TODO
+    App.$.squares.forEach((square) => {
+      square.addEventListener("click", (event) => {
+        // Check if there is already a play if so, return early
+        const hasMove = (squareId) => {
+          const existingMove = App.state.moves.find(
+            (move) => move.squareId === squareId
+          );
+          return existingMove !== undefined;
+        };
+
+        if (hasMove(+square.id)) {
+          return;
+        }
+
+        // Determine which player icon to add to the square
+        const lastMove = App.state.moves.at(-1);
+        const getOppositePlayer = (playerId) => (playerId === 1 ? 2 : 1);
+        const currentPlayer =
+          App.state.moves.length === 0
+            ? 1
+            : getOppositePlayer(lastMove.playerId);
+
+        const icon = document.createElement("i");
+
+        if (currentPlayer === 1) {
+          icon.classList.add("fa-solid", "fa-x", "yellow");
+        } else {
+          icon.classList.add("fa-solid", "fa-o", "turquoise");
+        }
+
+        App.state.moves.push({ squareId: +square.id, playerId: currentPlayer });
+
+        square.replaceChildren(icon);
+
+        console.log(App.state);
+
+        // Check if there is a winner or tie game
+        const winningPatters = [
+          [1, 2, 3],
+          [1, 5, 9],
+          [1, 4, 7],
+          [2, 5, 8],
+          [3, 5, 7],
+          [3, 6, 9],
+          [4, 5, 6],
+          [7, 8, 9],
+        ];
+      });
+    });
+  },
+};
+
+window.addEventListener("load", App.init);
